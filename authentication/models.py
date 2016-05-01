418a3a4deffa75f31a5ef3fa672ad46d49ae4aa7
Rich Torres <richjtorres@gmail.com>
2016-04-30 21:01:28 -0700

    adding account last name

--- authentication/models.py
+++ authentication/models.py
@@ -34,6 +34,7 @@ class Account(AbstractBaseUser):
     username = models.CharField(max_length=40, unique=True)
 
     first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
     tagline = models.CharField(max_length=140, blank=True)
 
     is_admin = models.BooleanField(default=False)
